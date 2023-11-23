006205fea3dc7dea7d9a88e07f7470e242e65138
Abduljalal lawal <72824981+B3Y0N4@users.noreply.github.com>
2023-11-23 21:32:37 +0300

    Add files via upload

---
+++ odev6/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:odev3_mobile_prog/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+
+
+
+
+
